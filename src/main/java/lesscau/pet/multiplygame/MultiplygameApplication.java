b72ddda0b8cd061bf91531e94f19ba903c5ca71a
Olga Goremykina <Olga_Goremykina@epam.com>
2022-05-21 12:52:23 +0300

    Initial commit

---
+++ src/main/java/lesscau/pet/multiplygame/MultiplygameApplication.java
@@ -0,0 +1,13 @@
+package lesscau.pet.multiplygame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiplygameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiplygameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lesscau/pet/multiplygame/MultiplygameApplicationTests.java
@@ -0,0 +1,13 @@
+package lesscau.pet.multiplygame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiplygameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
