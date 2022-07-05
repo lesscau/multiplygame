9db8714f1de85e8accd4bcdfeb9a305762b07d26
Olga Goremykina <Olga_Goremykina@epam.com>
2022-07-05 11:24:43 +0300

    [BE] Implement DAO

--- src/main/java/lesscau/pet/multiplygame/challenge/ChallengeAttempt.java
+++ src/main/java/lesscau/pet/multiplygame/challenge/ChallengeAttempt.java
@@ -3,11 +3,25 @@ package lesscau.pet.multiplygame.challenge;
 import lesscau.pet.multiplygame.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ChallengeAttempt {
+    @Id
+    @GeneratedValue
     private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
     private User user;
     private int factorA;
     private int factorB;
--- src/main/java/lesscau/pet/multiplygame/user/User.java
+++ src/main/java/lesscau/pet/multiplygame/user/User.java
@@ -2,10 +2,19 @@ package lesscau.pet.multiplygame.user;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue
     private int id;
     private String nickname;
 }
